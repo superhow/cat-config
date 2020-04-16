a043ca5e2027e63ff8f4c60b080caed4334ff67f
Vytautas Superhero <vytautas@techsuperheroes.ch>
2020-04-16 21:40:07 +0300

    remap folders

--- reset.sh
+++ reset.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-
 local network_type=$1
 local node_type=$2
 local catapult_bin=$3
@@ -11,7 +10,7 @@ local network_id=mijin
 local script_src=$PWD/scripts/cat-config
 
 if [[ -z "${network_type}" ]] then;
-    echo "script must be called with one of the three network options: local | existing | foundation"
+    echo "script must be called with one of the three network options: private | existing | symbol"
     return 0
     
     elif [[ -z "${node_type}" ]] then;
@@ -21,6 +20,15 @@ fi
 
 echo
 echo "Welcome to the Catapult Config Utility"
+echo
+echo "network_type = ${network_type}"
+echo "node_type = ${node_type}"
+echo "catapult_bin = ${catapult_bin}"
+echo "private_key = ${private_key}"
+echo "public_key = ${public_key}"
+echo "template = ${template}"
+echo "network_id = ${mijin}"
+echo "script_src = ${script_src}"
 echo
 
 # reapply data directory
@@ -66,61 +74,73 @@ mkdir $PWD/nemesis
 function setup_existing() {
     local generation_hash=$(grep "private key:" ${script_src}/templates/${template}/generation_hash.txt | sed 's/private key://g' | tr -d ' ')
     
+    echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/${template} $PWD/resources ${private_key} ${public_key} ${generation_hash}"
     source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/${template} $PWD/resources ${private_key} ${public_key} ${generation_hash}
     cp -R ${script_src}/templates/${template}/seed/* $PWD/data
 }
 
-function setup_local() {
+function setup_private() {
     
     echo
-    echo "Generating network generation hash (UUID)"
+    echo "Generating network generation hash (UUID) for local private network"
     echo
+    echo "${script_src}/generate_hash.sh ${catapult_bin} ${network_id}"
     source ${script_src}/generate_hash.sh ${catapult_bin} ${network_id}
     local generation_hash=$(grep "private key:" $PWD/generation_hash.txt | sed 's/private key://g' | tr -d ' ')
+    echo
+    echo "Generetion hash is: ${generation_hash}"
+    echo
     
     echo
     echo "Preparing resources"
     echo
-    source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/local $PWD/resources ${private_key} ${public_key} ${generation_hash} ${network_id}
+    echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/private $PWD/resources ${private_key} ${public_key} ${generation_hash} ${network_id}"
+    source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/private $PWD/resources ${private_key} ${public_key} ${generation_hash} ${network_id}
     
     echo
     echo "Generating new nemesis block"
     echo
+    echo "${script_src}/prepare_nemesis_block.sh ${catapult_bin} ${private_key} ${generation_hash} ${network_id}"
     source ${script_src}/prepare_nemesis_block.sh ${catapult_bin} ${private_key} ${generation_hash} ${network_id}
 }
 
-function setup_foundation() {
+function setup_symbol() {
     echo
-    echo "Preparing foundation resources"
+    echo "Preparing Symbol public resources"
     echo
-    # Generation hash and network public key from configuration for the foundation network
+    # Generation hash and network public key from configuration for the Symbol network
     local generation_hash=CC42AAD7BD45E8C276741AB2524BC30F5529AF162AD12247EF9A98D6B54A385B
     local network_public_key=A3CE86263CD000F45867A6B5A396A521AF4557D9A6BD3C796478A9BF40BF4F4C
-    source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/foundation $PWD/resources ${private_key} ${network_public_key} ${generation_hash}
+    echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/symbol $PWD/resources ${private_key} ${network_public_key} ${generation_hash}"
+    source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/symbol $PWD/resources ${private_key} ${network_public_key} ${generation_hash}
     
-    cp -R ${script_src}/templates/foundation/seed/* $PWD/data
-    cp -R ${script_src}/templates/foundation/seed/* $PWD/seed
-    
-    echo "Finished."
+    cp -R ${script_src}/templates/symbol/seed/* $PWD/data
+    cp -R ${script_src}/templates/symbol/seed/* $PWD/seed
 }
 
-while [[ 0 -ne $# ]]; do
-    case "${network_type}" in
-        ## Prepare a standalone, single local node with its own completely new network
-        local)
-           
-            setup_local
-        ;;
-        ## Prepare a node that is capable of connecting to the Foundation network
-        foundation)
-            
-            setup_foundation
-        ;;
-        ## Prepare a node that is ready to connect to an existing network
-        existing)
-            
-            setup_existing
-        ;;
-    esac
-    shift
-done
+case "${network_type}" in
+    ## Prepare a standalone, single private local node with its own completely new network
+    private)
+         
+        setup_private
+        echo
+        echo "Finished setup local private."
+        echo
+    ;;
+    ## Prepare a node that is capable of connecting to the Symbol public network
+    symbol)
+        
+        setup_symbol
+        echo
+        echo "Finished setup Symbol public."
+        echo
+    ;;
+    ## Prepare a node that is ready to connect to an existing network
+    existing)
+        
+        setup_existing
+        echo
+        echo "Finished setup existing."
+        echo
+    ;;
+esac
