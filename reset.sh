4b403de9e91f34d451f2acb32f06fe7b71eb346d
Vytautas Superhero <vytautas@techsuperheroes.ch>
2020-04-16 23:25:39 +0300

    fix

--- reset.sh
+++ reset.sh
@@ -27,7 +27,7 @@ echo "catapult_bin = ${catapult_bin}"
 echo "private_key = ${private_key}"
 echo "public_key = ${public_key}"
 echo "template = ${template}"
-echo "network_id = ${mijin}"
+echo "network_id = ${network_id}"
 echo "script_src = ${script_src}"
 echo
 
@@ -74,6 +74,7 @@ mkdir $PWD/nemesis
 function setup_existing() {
     local generation_hash=$(grep "private key:" ${script_src}/templates/${template}/generation_hash.txt | sed 's/private key://g' | tr -d ' ')
     
+    echo
     echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/${template} $PWD/resources ${private_key} ${public_key} ${generation_hash}"
     source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/${template} $PWD/resources ${private_key} ${public_key} ${generation_hash}
     cp -R ${script_src}/templates/${template}/seed/* $PWD/data
@@ -95,12 +96,14 @@ function setup_private() {
     echo "Preparing resources"
     echo
     echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/private $PWD/resources ${private_key} ${public_key} ${generation_hash} ${network_id}"
+    echo
     source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/private $PWD/resources ${private_key} ${public_key} ${generation_hash} ${network_id}
     
     echo
     echo "Generating new nemesis block"
     echo
     echo "${script_src}/prepare_nemesis_block.sh ${catapult_bin} ${private_key} ${generation_hash} ${network_id}"
+    echo
     source ${script_src}/prepare_nemesis_block.sh ${catapult_bin} ${private_key} ${generation_hash} ${network_id}
 }
 
@@ -112,6 +115,7 @@ function setup_symbol() {
     local generation_hash=CC42AAD7BD45E8C276741AB2524BC30F5529AF162AD12247EF9A98D6B54A385B
     local network_public_key=A3CE86263CD000F45867A6B5A396A521AF4557D9A6BD3C796478A9BF40BF4F4C
     echo "${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/symbol $PWD/resources ${private_key} ${network_public_key} ${generation_hash}"
+    echo
     source ${script_src}/prepare_resources.sh ${node_type} ${catapult_bin} ${script_src}/templates/symbol $PWD/resources ${private_key} ${network_public_key} ${generation_hash}
     
     cp -R ${script_src}/templates/symbol/seed/* $PWD/data
