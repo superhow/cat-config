9429bff1212603b5bf61fd3e3f83a4ddc4fbe39b
Vytautas K <ministras@users.noreply.github.com>
2020-05-27 22:37:54 +0300

    vrf keys

--- generate_hash.sh
+++ generate_hash.sh
@@ -2,8 +2,9 @@
 
 local catapult_bin=$1
 local generation_path=$PWD/generation_hash.txt
+local vrfkeys_path=$PWD/vrf_keys.txt
 local network_id=$2
 
 ## Run address tool
-# ${catapult_bin}/bin/catapult.tools.address -n mijin-test -g 1 > "${generation_path}"
 ${catapult_bin}/bin/catapult.tools.address -n $network_id -g 1 > "$generation_path"
+${catapult_bin}/bin/catapult.tools.address -n $network_id -g 5 > "$vrfkeys_path"
