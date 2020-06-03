e8853eb5570006f425a52870cb4f6e969df9b1df
Vytautas K <ministras@users.noreply.github.com>
2020-06-03 20:31:14 +0300

    Update generate_hash.sh

--- generate_hash.sh
+++ generate_hash.sh
@@ -7,4 +7,4 @@ local network_id=$2
 
 ## Run address tool
 ${catapult_bin}/bin/catapult.tools.address -n $network_id -g 1 > "$generation_path"
-${catapult_bin}/bin/catapult.tools.address -n $network_id -g 5 > "$vrfkeys_path"
+${catapult_bin}/bin/catapult.tools.address -n $network_id -g 3 > "$vrfkeys_path"
